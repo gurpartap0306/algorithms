85f4115b384cd5ca2368e0aef832525b4981b9dc
gurpartap0306 <dhaliwal.gurpartap@gmail.com>
2020-03-03 12:09:39 +0530

    updated linklist

--- data_structure/linkedlist.py
+++ data_structure/linkedlist.py
@@ -9,22 +9,39 @@ class linkedlist:
 
 def insert(l,a):
     n=node(a)
+    t=l.head
     if(l.head==None):
         l.head=n
     else:
-        n.next=l.head
-        l.head=n
+        while t.next != None:
+            t=t.next
+        t.next=n
 
 def traverse(l):
     while l.head!=None:
         print(l.head.data)
         l.head=l.head.next
 
+def delete(l,a):
+    t=l.head
+    if t==None:
+        print('data underflow')
+    else:
+        i=0
+        while i<a-1 and t!=None:
+            t=t.next
+            i+=1
+        if t!=None:
+            t1=t.next
+            t.next=t1.next
+
+
 def main():
     listt=linkedlist()
     for i in range(5):
         insert(listt,input())
     print('*********')
+    delete(listt,3)
     traverse(listt)
 
 if __name__=='__main__':
