b819a20e42c715d21ae2823988ad73eacfc8c3ad
gurpartap0306 <dhaiwal.gurpartap@gmail.com>
2020-03-04 18:03:03 +0530

    binary tree

--- data_structure/binaryTree.py
+++ data_structure/binaryTree.py
@@ -38,6 +38,32 @@ def search(t,key):
         else: a=search(t.right,key)
     return a
 
+def min(t):
+    while t.left !=None:
+        t=t.left
+    return t
+
+def delete(t,key):
+    if t==None:
+        return t
+    elif key<t.key: t.left=delete(t.left,key)
+    elif key>t.key: t.right=delete(t.right,key)
+    else:
+        if t.left is None:
+            temp=t.right
+            t=None
+            return temp
+        elif t.right is None:
+            temp=t.left
+            t=None
+            return temp
+        temp=min(t.right)
+        t.key=temp.key
+        t.right=delete(t.right,temp.key)
+    return t
+
+
+
 def main():
     t=tree()
     insert(t,6)
@@ -46,10 +72,12 @@ def main():
     insert(t,7)
     insert(t,8)
     insert(t,5)
+    #inorder(t.root)
+    t.root=delete(t.root,6)
     inorder(t.root)
-    a=search(t.root,5)
-    if a!=None: print(a.key)
-    else:print ('no element in tree')
+    #a=search(t.root,5)
+    #if a!=None: print(a.key)
+    #else:print ('no element in tree')
 
 
 if __name__=='__main__':
