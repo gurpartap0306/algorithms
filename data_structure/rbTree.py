bcf0ac123b21df13e09d45f7c19caf68b1f43a5d
gurpartap0306 <dhaliwal.gurpartap@gmail.com>
2020-03-05 17:03:01 +0530

    red-black tree

---
+++ data_structure/rbTree.py
@@ -0,0 +1,133 @@
+z=None
+
+class node:
+    def __init__(self,key):
+        self.key=key
+        self.color='black'
+        self.p=None
+        self.left=None
+        self.right=None
+
+class tree:
+    def __init__(self):
+        self.root=node(None)
+
+def left_rotate(t,z):
+    y=z.right
+    z.right=y.left
+    if y.left.key==None:
+        y.left.p=z
+    y.p=z.p
+    if z.p.key==None:
+        t.root=y
+    elif z==z.p.left:
+        z.p.left=y
+    else: z.p.right =y
+    y.left =z
+    z.p =y
+
+def right_rotate(t,z):
+    y=z.left
+    z.left=y.left
+    if y.left.key!=None:
+        y.left.p=z
+    y.p=z.p
+    if z.p.key==None:
+        t.root=y
+    elif z==z.p.left:
+        z.p.left=y
+    else: z.p.right =y
+    y.right =z
+    z.p =y
+
+'''def rb_insert_fixup(t,z):
+    while z.p.color=='red':
+        print('hi')
+        if z.p==z.p.p.left:
+            y=z.p.p.right
+            if y.color == 'red':
+                z.p.color='black'
+                y.color='black'
+                z.p.p.color='red'
+                z=z.p.p
+            elif z==z.p.right:
+                z=z.p
+                left_rotate(t,z)
+                z.p.color='black'
+                z.p.p.color='red'
+                right_rotate(t,z)
+
+        else:
+            y=z.p.p.left
+            if y.color=='red':
+                z.p.color='black'
+                y.color='black'
+                z.p.p.color='red'
+                z=z.p.p
+            elif z==z.p.left:
+                z=z.p
+                right_rotate(t,z)
+                z.p.color='black'
+                z.p.p.color='red'
+                left_rotate(t,z)
+    t.root.color='black'
+'''
+
+def rb_insert(t,z):
+    y=node(None)
+    x=t.root
+    while x.key != None :
+        y=x
+        if z.key < x.key:
+            x=x.left
+        else: x=x.right
+    #if x is not None: print(type(z.key))
+    z.p=y
+    if y.key==None:
+        t.root=z
+    elif z.key < y.key:
+        y.left=z
+    else: y.right=z
+    z.left=node(None)
+    z.right=node(None)
+    z.color='red'
+    #rb_insert_fixup(t,z)
+
+def inorder(t):
+    if t is not None:
+        inorder(t.left)
+        if t.key is not None: print(t.key)
+        inorder(t.right)
+
+def search(t,z):
+    if t.key ==None:
+        return t
+    if t.key==z: return t
+    elif z<t.key:
+        a= search(t.left,z)
+    else: a= search(t.right,z)
+    return a
+
+
+def main():
+    t=tree()
+    rb_insert(t,node(2))
+    rb_insert(t,node(1))
+    rb_insert(t,node(4))
+    rb_insert(t,node(5))
+    rb_insert(t,node(9))
+    rb_insert(t,node(3))
+    rb_insert(t,node(6))
+    rb_insert(t,node(7))
+    rb_insert(t,node(15))
+    #print(t.root.key)
+    z=search(t.root,9)
+    print (z.p)
+    #inorder(t.root)
+    print('********')
+    left_rotate(t,z)
+    #inorder(t.root)
+    print(search(t.root,9).p)
+
+if __name__=='__main__':
+    main()
