43734d245dcc3fdd41c0469c08bbe30c9a9cec5b
gurpartap0306 <dhaliwal.gurpartap@gmail.com>
2020-03-09 22:28:25 +0530

    rod_cutting

--- dynamic_programming/rod_cutting.py
+++ dynamic_programming/rod_cutting.py
@@ -1,9 +1,12 @@
 i=[1,2,3,4,5,6,7,8,9,10]
 p=[1,5,8,9,10,17,17,20,24,30]
 
+def rod_cut(p,n):
+
+
 def main():
     n=50
-    price=rod+cut(p,n)
+    price=rod_cut(p,n)
 
 
 if __name__=='__main__':
