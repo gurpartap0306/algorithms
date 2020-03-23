5ad301bdfb1a16e0ec8075eea09013f7995c7518
gurpartap0306 <dhaliwal.gurpartap@gmail.com>
2020-03-23 21:24:57 +0530

    recursive method

--- dynamic_programming/lcs.py
+++ dynamic_programming/lcs.py
@@ -1,14 +1,17 @@
-def lcs(a,b):
+
+# A Naive recursive Python implementation of LCS problem
+
+def lcs(X, Y, m, n):
+
+    if m == 0 or n == 0:
+       return 0;
+    elif X[m-1] == Y[n-1]:
+       return 1 + lcs(X, Y, m-1, n-1);
+    else:
+       return max(lcs(X, Y, m, n-1), lcs(X, Y, m-1, n));
 
 
-
-def main():
-    a='abcdef'
-    b='cdef'
-    c=lcs(a,b)
-
-if __name__=='__main__':
-    main()
-
-
-#i have to figure out how to slove this problem .
+# Driver program to test the above function
+X = "AGGTAB"
+Y = "GXTXAYB"
+print ("Length of LCS is ", lcs(X , Y, len(X), len(Y)))
